fd34e251dbe11efd91378c24d03ebd9ab15d6593
ch-egli <chrsitian.egli@gmx.net>
2016-12-02 10:57:52 +0100

    caching of authentication objects added

--- src/main/java/ch/egli/auth/CachingLdapAuthenticationProvider.java
+++ src/main/java/ch/egli/auth/CachingLdapAuthenticationProvider.java
@@ -1,10 +1,12 @@
 package ch.egli.auth;
 
+import org.apache.wss4j.common.ext.WSSecurityException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
@@ -40,11 +42,19 @@ public class CachingLdapAuthenticationProvider extends LdapAuthenticationProvide
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         String userName = authentication.getName();
+        String password = (String) authentication.getCredentials();
 
         // return Authentication object for current user from cache, if it is available
         Authentication result = cache.get(userName, Authentication.class);
         if (result != null) {
             LOGGER.debug("### found auth obj for user '{}' in cache", userName);
+
+            // check that password matches...
+            String pwFromCache = (String) result.getCredentials();
+            if (!password.equals(pwFromCache)) {
+                LOGGER.error("Authentication failed for user '{}'. Credentials in cache do not match the given credentials!", userName);
+                throw new BadCredentialsException("Credentials in cache do not match the credentials given by the current user '" + userName + "'");
+            }
             return result;
         }
 
